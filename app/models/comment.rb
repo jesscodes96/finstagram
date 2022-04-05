f4253fc3c6dfe1997757423f182ef2226cc3ed1b
jesscodes96 <jess96sim@gmail.com>
2022-04-05 00:38:14 +0000

    W6D1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,3 +4,11 @@ class Comment < ActiveRecord::Base
     belongs_to :finstagram_post
     
 end
+
+class Comment < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :finstagram_post
+
+    validates_presence_of :text, :user, :finstagram_post
+
+end
